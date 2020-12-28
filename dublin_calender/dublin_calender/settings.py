d2de29686bc15182724441cd437bafc6e9a8a1df
sachsom95 <sachin.soman@ucdconnect.ie>
2020-12-28 01:37:23 +0000

    Added the login template - Created profile app - route to profile/login url - Rendered login page template

--- dublin_calender/dublin_calender/settings.py
+++ dublin_calender/dublin_calender/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Profiles template register
+    'profiles.apps.ProfilesConfig',
+
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
