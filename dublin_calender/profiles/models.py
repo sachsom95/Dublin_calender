f9ce01f6bae50b7e4ba68de421d49076e826af3c
sachsom95 <sachin.soman@ucdconnect.ie>
2020-12-28 03:50:09 +0000

    Created basic Calender model - registered with admin

--- dublin_calender/profiles/models.py
+++ dublin_calender/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Calender(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.CharField(max_length=200, default='')
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
