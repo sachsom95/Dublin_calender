160749b34db17976d78c0fc79b5163e3171026bf
sachsom95 <sachin.soman@ucdconnect.ie>
2020-12-30 00:06:46 +0000

    Added all the required buttons

--- dublin_calender/dublin_calender/urls.py
+++ dublin_calender/dublin_calender/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='profiles/logout.html'), name='logout'),
     path('register/', profile_views.register, name='register'),
     path('home/', include('calender.urls')),
-
+    path('', include('calender.urls')),
 ]
